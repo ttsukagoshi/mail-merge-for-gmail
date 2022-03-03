b6c7f8c1fc1e2129724822b6ac44e5b3820e6ec8
ttsukagoshi <taro.tsukagoshi@gmail.com>
2022-03-03 12:42:01 +0900

    Integrate contents of src/i18n.js to mailMerge.js

--- test/getKnownErrorMessages_.test.js
+++ test/getKnownErrorMessages_.test.js
@@ -1,10 +1,9 @@
 const mailMerge = require('../src/mailMerge');
-const i18n = require('../src/i18n');
 
-Object.keys(i18n.MESSAGES).forEach((locale) => {
-  let matchArray = Object.keys(i18n.MESSAGES[locale])
+Object.keys(mailMerge.MESSAGES).forEach((locale) => {
+  let matchArray = Object.keys(mailMerge.MESSAGES[locale])
     .filter((messageKey) => messageKey.slice(0, 5) === 'error')
-    .map((key) => i18n.MESSAGES[locale][key]);
+    .map((key) => mailMerge.MESSAGES[locale][key]);
   test('getKnownErrorMessages_', () => {
     expect(mailMerge.getKnownErrorMessages_(locale)).toEqual(matchArray);
   });
