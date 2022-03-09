5a0db32cce796f8bc7e80ec519799ce17b245b76
ttsukagoshi <taro.tsukagoshi@gmail.com>
2022-03-10 04:35:29 +0900

    Modulize mock add-on event objects and configs

---
+++ test/mock-configs.js
@@ -0,0 +1,173 @@
+const mockConfigs = [
+  {
+    eventName: 'Default settings',
+    addonEvent: {
+      commonEventObject: {
+        userLocale: 'ja',
+        formInputs: {
+          DATA_SHEET_NAME: { stringInputs: { value: ['demo sheet name 123'] } },
+          GROUP_FIELD_MARKER_TEXT: {
+            stringInputs: { value: ['\\[\\[([^\\]]+)\\]\\]'] },
+          },
+          MERGE_FIELD_MARKER_TEXT: {
+            stringInputs: { value: ['\\{\\{([^}]+)\\}\\}'] },
+          },
+          REPLY_TO: { stringInputs: { value: ['replyTo@email.com'] } },
+          REPLACE_VALUE: { stringInputs: { value: ['NA'] } },
+          TO: { stringInputs: { value: ['{{Email}}'] } },
+          ENABLE_GROUP_MERGE: { stringInputs: { value: ['enabled'] } },
+          SPREADSHEET_URL: {
+            stringInputs: {
+              value: [
+                'https://docs.google.com/spreadsheets/d/qwerty12345/edit',
+              ],
+            },
+          },
+          ROW_INDEX_MARKER: { stringInputs: { value: ['{{i}}'] } },
+        },
+        timeZone: { offset: 32400000, id: 'Asia/Dili' },
+        hostApp: 'GMAIL',
+        platform: 'WEB',
+      },
+    },
+    parsedConfig: {
+      SPREADSHEET_URL:
+        'https://docs.google.com/spreadsheets/d/qwerty12345/edit',
+      DATA_SHEET_NAME: 'demo sheet name 123',
+      TO: '{{Email}}',
+      CC: '',
+      BCC: '',
+      TEMPLATE_SUBJECT: '',
+      ENABLE_GROUP_MERGE: true,
+      ENABLE_REPLY_TO: false,
+      REPLY_TO: 'replyTo@email.com',
+      REPLACE_VALUE: 'NA',
+      MERGE_FIELD_MARKER: /\{\{([^}]+)\}\}/g,
+      MERGE_FIELD_MARKER_TEXT: '\\{\\{([^}]+)\\}\\}',
+      GROUP_FIELD_MARKER: /\[\[([^\]]+)\]\]/g,
+      GROUP_FIELD_MARKER_TEXT: '\\[\\[([^\\]]+)\\]\\]',
+      ROW_INDEX_MARKER: '{{i}}',
+      ENABLE_DEBUG_MODE: false,
+      hostApp: 'GMAIL',
+      userLocale: 'ja',
+    },
+  },
+  {
+    eventName: 'Settings all enabled',
+    addonEvent: {
+      commonEventObject: {
+        userLocale: 'ja',
+        hostApp: 'GMAIL',
+        timeZone: { id: 'Asia/Dili', offset: 32400000 },
+        platform: 'WEB',
+        formInputs: {
+          TEMPLATE_SUBJECT: {
+            stringInputs: { value: ['test draft demo {{Name}}'] },
+          },
+          MERGE_FIELD_MARKER_TEXT: {
+            stringInputs: { value: ['\\{\\{([^}]+)\\}\\}'] },
+          },
+          ENABLE_GROUP_MERGE: { stringInputs: { value: ['enabled'] } },
+          ENABLE_REPLY_TO: { stringInputs: { value: ['enabled'] } },
+          REPLACE_VALUE: { stringInputs: { value: ['NA'] } },
+          ROW_INDEX_MARKER: { stringInputs: { value: ['{{i}}'] } },
+          CC: { stringInputs: { value: ['{{cc}}'] } },
+          GROUP_FIELD_MARKER_TEXT: {
+            stringInputs: { value: ['\\[\\[([^\\]]+)\\]\\]'] },
+          },
+          TO: { stringInputs: { value: ['{{Email}}'] } },
+          SPREADSHEET_URL: {
+            stringInputs: {
+              value: [
+                'https://docs.google.com/spreadsheets/d/qwerty12345/edit',
+              ],
+            },
+          },
+          DATA_SHEET_NAME: { stringInputs: { value: ['demo sheet name 123'] } },
+          REPLY_TO: { stringInputs: { value: ['replyTo@email.com'] } },
+          ENABLE_DEBUG_MODE: { stringInputs: { value: ['enabled'] } },
+          BCC: { stringInputs: { value: ['{{bcc}}'] } },
+        },
+      },
+    },
+    parsedConfig: {
+      SPREADSHEET_URL:
+        'https://docs.google.com/spreadsheets/d/qwerty12345/edit',
+      DATA_SHEET_NAME: 'demo sheet name 123',
+      TO: '{{Email}}',
+      CC: '{{cc}}',
+      BCC: '{{bcc}}',
+      TEMPLATE_SUBJECT: 'test draft demo {{Name}}',
+      ENABLE_GROUP_MERGE: true,
+      ENABLE_REPLY_TO: true,
+      REPLY_TO: 'replyTo@email.com',
+      REPLACE_VALUE: 'NA',
+      MERGE_FIELD_MARKER: /\{\{([^}]+)\}\}/g,
+      MERGE_FIELD_MARKER_TEXT: '\\{\\{([^}]+)\\}\\}',
+      GROUP_FIELD_MARKER: /\[\[([^\]]+)\]\]/g,
+      GROUP_FIELD_MARKER_TEXT: '\\[\\[([^\\]]+)\\]\\]',
+      ROW_INDEX_MARKER: '{{i}}',
+      ENABLE_DEBUG_MODE: true,
+      hostApp: 'GMAIL',
+      userLocale: 'ja',
+    },
+  },
+  {
+    eventName: 'Settings all disabled',
+    addonEvent: {
+      commonEventObject: {
+        formInputs: {
+          REPLY_TO: { stringInputs: { value: ['replyTo@email.com'] } },
+          REPLACE_VALUE: { stringInputs: { value: ['NA'] } },
+          ROW_INDEX_MARKER: { stringInputs: { value: ['{{i}}'] } },
+          TEMPLATE_SUBJECT: {
+            stringInputs: { value: ['test draft demo {{Name}}'] },
+          },
+          MERGE_FIELD_MARKER_TEXT: {
+            stringInputs: { value: ['\\{\\{([^}]+)\\}\\}'] },
+          },
+          SPREADSHEET_URL: {
+            stringInputs: {
+              value: [
+                'https://docs.google.com/spreadsheets/d/qwerty12345/edit',
+              ],
+            },
+          },
+          GROUP_FIELD_MARKER_TEXT: {
+            stringInputs: { value: ['\\[\\[([^\\]]+)\\]\\]'] },
+          },
+          BCC: { stringInputs: { value: ['{{bcc}}'] } },
+          TO: { stringInputs: { value: ['{{Email}}'] } },
+          CC: { stringInputs: { value: ['{{cc}}'] } },
+          DATA_SHEET_NAME: { stringInputs: { value: ['demo sheet name 123'] } },
+        },
+        userLocale: 'ja',
+        platform: 'WEB',
+        timeZone: { offset: 32400000, id: 'Asia/Dili' },
+        hostApp: 'GMAIL',
+      },
+    },
+    parsedConfig: {
+      SPREADSHEET_URL:
+        'https://docs.google.com/spreadsheets/d/qwerty12345/edit',
+      DATA_SHEET_NAME: 'demo sheet name 123',
+      TO: '{{Email}}',
+      CC: '{{cc}}',
+      BCC: '{{bcc}}',
+      TEMPLATE_SUBJECT: 'test draft demo {{Name}}',
+      ENABLE_GROUP_MERGE: false,
+      ENABLE_REPLY_TO: false,
+      REPLY_TO: 'replyTo@email.com',
+      REPLACE_VALUE: 'NA',
+      MERGE_FIELD_MARKER: /\{\{([^}]+)\}\}/g,
+      MERGE_FIELD_MARKER_TEXT: '\\{\\{([^}]+)\\}\\}',
+      GROUP_FIELD_MARKER: /\[\[([^\]]+)\]\]/g,
+      GROUP_FIELD_MARKER_TEXT: '\\[\\[([^\\]]+)\\]\\]',
+      ROW_INDEX_MARKER: '{{i}}',
+      ENABLE_DEBUG_MODE: false,
+      hostApp: 'GMAIL',
+      userLocale: 'ja',
+    },
+  },
+];
+module.exports = { mockConfigs };
